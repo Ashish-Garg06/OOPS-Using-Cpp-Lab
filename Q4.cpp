56e41ed37933e4cc4990933328af87ee048420f3
Ashish Garg <ashishgarg0005@gmail.com>
2023-02-04 10:56:57 +0530

    Q 4 initial commit

---
+++ Q4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number of lines to print the pattern (up to n lines): ";
+    cin >> n;
+
+    //  This loop will run n times, where n is the number of lines to print 
+    for (int i = 1; i <= n; i++)
+    {
+        //  This variable k will be used to print the numbers 
+        int k = 1;
+
+        //  This loop will run n times for first line 
+        // n-1 times for second line and so on till it reaches last line 
+        // when it will run for 1 time
+        for (int j = n; j >=(i); j--)
+        {
+            //  Print the current value of k and a space after it 
+            cout << k << " ";
+
+            //  Increase k by 1 each time this loop runs 
+            k++;
+        }
+
+        //  Move to a new line after each line is printed 
+        cout << "\n";
+    }
+
+    return 0;
+}
