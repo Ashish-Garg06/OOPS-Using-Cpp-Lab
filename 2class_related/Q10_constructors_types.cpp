a95e1eb8a895e7c6c59c86d0ced56884f6beaad3
Ashish Garg <ashishgarg5006@gmail.com>
2023-03-29 14:07:44 +0530

    updated code according to the question

--- 2class_related/Q10_constructors_types.cpp
+++ 2class_related/Q10_constructors_types.cpp
@@ -3,48 +3,59 @@ using namespace std;
 
 class rectangle
 {
-    private:
-        int length, breadth;
-    public:
-        rectangle(void);
-        
-        void getdata(void);
-        void setdata(int a, int b);
-        int getarea(void);
+private:
+    int length, breadth;
 
+public:
+    rectangle(void);
+    rectangle(int a, int b);
+
+    void getdata(void);
+    void setdata(int a, int b);
+    int getarea(void);
 };
 
 int main()
 {
-    rectangle x;
-    x.getdata();
-    x.setdata(245,20);
-    x.getdata();
+    rectangle obj1;
+    obj1.getdata();
+    obj1.setdata(245, 20);
+    obj1.getdata();
 
-    cout <<x.getarea();
+    cout << "area of obj 1 = " << obj1.getarea() << endl;
+
+    rectangle obj2(30, 25);
+    obj2.getdata();
+    cout << "area of obj 2 = " << obj2.getarea() << endl;
 
     return 0;
 }
 
 rectangle::rectangle(void)
 {
-    length=0;
-    breadth=0;
+    length = 0;
+    breadth = 0;
+}
+
+rectangle::rectangle(int a, int b)
+{
+    length = a;
+    breadth = b;
 }
 
 void rectangle::getdata(void)
 {
-    cout<<"length : "<<length<<"\n";
-    cout<<"breadth : "<<breadth<<"\n";
+    cout << "length : " << length << "\n";
+    cout << "breadth : " << breadth << "\n";
 }
 
 void rectangle::setdata(int a, int b)
 {
-    length=a;
-    breadth=b;
+    length = a;
+    breadth = b;
 }
 
 int rectangle::getarea(void)
 {
-    return (length*breadth);
+    return (length * breadth);
 }
