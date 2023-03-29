7eb4c00097d064edeb029d08ad032ab112b3565e
Ashish Garg <ashishgarg5006@gmail.com>
2023-03-29 14:10:05 +0530

    Capitalized class name

--- 2class_related/Q10_constructors_types.cpp
+++ 2class_related/Q10_constructors_types.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-class rectangle
+class Rectangle
 {
 private:
     int length, breadth;
 
 public:
-    rectangle(void);
-    rectangle(int a, int b);
+    Rectangle(void);
+    Rectangle(int a, int b);
 
     void getdata(void);
     void setdata(int a, int b);
@@ -17,45 +17,45 @@ public:
 
 int main()
 {
-    rectangle obj1;
+    Rectangle obj1;
     obj1.getdata();
     obj1.setdata(245, 20);
     obj1.getdata();
 
     cout << "area of obj 1 = " << obj1.getarea() << endl;
 
-    rectangle obj2(30, 25);
+    Rectangle obj2(30, 25);
     obj2.getdata();
     cout << "area of obj 2 = " << obj2.getarea() << endl;
 
     return 0;
 }
 
-rectangle::rectangle(void)
+Rectangle::Rectangle(void)
 {
     length = 0;
     breadth = 0;
 }
 
-rectangle::rectangle(int a, int b)
+Rectangle::Rectangle(int a, int b)
 {
     length = a;
     breadth = b;
 }
 
-void rectangle::getdata(void)
+void Rectangle::getdata(void)
 {
     cout << "length : " << length << "\n";
     cout << "breadth : " << breadth << "\n";
 }
 
-void rectangle::setdata(int a, int b)
+void Rectangle::setdata(int a, int b)
 {
     length = a;
     breadth = b;
 }
 
-int rectangle::getarea(void)
+int Rectangle::getarea(void)
 {
     return (length * breadth);
 }
