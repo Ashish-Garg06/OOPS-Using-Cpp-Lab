087cfb1295ef459f4034bb2e1037fbba7521fa67
Ashish Garg <ashishgarg5006@gmail.com>
2023-04-29 12:57:31 +0530

    added Q4.cpp

---
+++ Assignment_2/Q4class_pointer.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Vehicle
+{
+private:
+    int speed;
+
+public:
+    void set_speed(int s)
+    {
+        speed = s;
+    }
+
+    void show_speed()
+    {
+        cout << "Speed of the vehicle: " << speed << endl;
+    }
+};
+
+int main()
+{
+    Vehicle car;
+    car.set_speed(120);
+
+    // Creating a pointer to class
+    Vehicle *vehiclePtr;
+    // Assigning the address of the 'car' object to the pointer 'vehiclePtr'
+    vehiclePtr = &car;
+
+    // Accessing class members using the pointer to class
+    vehiclePtr->show_speed();
+    // Changing the speed using the pointer to class
+    vehiclePtr->set_speed(150);
+    vehiclePtr->show_speed();
+
+    // Creating a class object using 'new'
+    Vehicle *bike = new Vehicle();
+    // Assigning values and accessing class members using pointer to object
+    bike->set_speed(90);
+    bike->show_speed();
+    // Releasing the memory allocated by 'new'
+    delete bike;
+
+    return 0;
+}
